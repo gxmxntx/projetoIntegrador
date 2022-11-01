549e690479d8cccd3c81af0d94a880707444da8d
gxmxntx <gabrielxgemente@gmail.com>
2022-11-01 11:07:57 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// function ativarMenu () {
+//     var menu = document.getElementById("menuSanduba");
+//     menu.classList.remove("fa-bars");
+//     menu.classList.add("fa-close")
+//     var menuDado = document.getElementById("menuFixed")
+//     menuDado.classList.remove("dNone")
+// }
+
+// function desativarMenu () {
+//     var menu = document.getElementById("menuSanduba");
+//     menu.classList.remove("fa-close");
+//     menu.classList.add("fa-bars")
+//     var menuDado = document.getElementById("menuFixed")
+//     menuDado.classList.add("dNone")
+// }
